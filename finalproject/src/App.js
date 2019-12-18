bead66b88ccbf1c1a9faccfc4309ee51e7d00d09
qkrdptjd2000 <qkrdptjd2000@snu.ac.kr>
2019-12-18 23:44:46 +0900

    Add match page

--- finalproject/src/App.js
+++ finalproject/src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Search from "./components/search/search";
+import Home from "./components/home/home";
+import SignUp from "./components/signup/signup";
+import MyPage from "./components/mypage/mypage";
+import Match from "./components/match/match";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        {/* <Route path="뒤에 들어갈 주소를 적어주세요. ex) signup">
+            주소에 접속시 나타나는 컴포넌트를 넣어주시면 됩니다.
+          </Route> */}
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/search/:place">
+          <Search />
+        </Route>
+        <Route path="/mypage">
+          <MyPage />
+        </Route>
+        <Route path="/match">
+          <Match />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ finalproject/src/components/match/match.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './match.css';
+
+const Match = () => {
+  return <div>매칭페이지</div>;
+};
+
+export default Match;
